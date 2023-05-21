4a63bb5a5616ee9950cebb6226561946b0feef8c
ASHADSAIFI003 <133789606+ASHADSAIFI003@users.noreply.github.com>
2023-05-21 19:04:31 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					` https://freebuzzs.rf.gd` + encodeURI(path as string)
+					` https://freebuzzs.rf.gd/` + encodeURI(path as string)
 				}`,
 			},
 		};
